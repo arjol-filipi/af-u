cf30d59fe692ea7af6f60dc4480fb4978246b9d4
arjol-filipi <filipi_arjol@yahoo.com>
2020-02-04 13:51:03 +0100

    no model start

--- updater.py
+++ updater.py
@@ -5,12 +5,12 @@ from news.sc import scrappTop,scrappKlan
 
 
 sched = BlockingScheduler()
-# @sched.scheduled_job( 'interval', minutes=30)
-# def sct():  
-#     scrappTop()
-# @sched.scheduled_job( 'interval', minutes=10)
-# def sck():
-#     scrappKlan()
+@sched.scheduled_job( 'interval', minutes=30)
+def sct():  
+    scrappTop()
+@sched.scheduled_job( 'interval', minutes=10)
+def sck():
+    scrappKlan()
 # @sched.scheduled_job('interval', days=1)
 # def delO():
     # deleteOld()
