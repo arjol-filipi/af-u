2ff9673a600c8176ae19fb908327ae111c56450c
arjol-filipi <filipi_arjol@yahoo.com>
2020-02-04 12:50:20 +0100

    se

--- updater.py
+++ updater.py
@@ -2,14 +2,21 @@ from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from news.sc import scrappTop,scrappKlan,deleteOld
 
+import os 
+
+os.environ["DJANGO_SETTINGS_MODULE"]="aflp.settings"
+
 sched = BlockingScheduler()
-@sched.scheduled_job( 'interval', minutes=30)
-def sct():  
-    scrappTop()
-@sched.scheduled_job( 'interval', minutes=10)
+# @sched.scheduled_job( 'interval', minutes=30)
+# def sct():  
+#     scrappTop()
+# @sched.scheduled_job( 'interval', minutes=10)
+# def sck():
+#     scrappKlan()
+# @sched.scheduled_job('interval', days=1)
+# def delO():
+    # deleteOld()
+@sched.scheduled_job( 'interval', minutes=1)
 def sck():
-    scrappKlan()
-@sched.scheduled_job('interval', days=1)
-def delO():
-    deleteOld()
+    print("   good   ")
 sched.start()
